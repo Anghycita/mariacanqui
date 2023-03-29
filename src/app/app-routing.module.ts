77db7697409c801180a215458ba741560a8696b6
Anghycita <angeladelpilar77@gmail.com>
2023-03-29 00:10:09 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'canqui', loadChildren: () => import('./canqui/canqui.module').then(m => m.CanquiModule) }];
+const routes: Routes = [
+  { path: 'canqui', loadChildren: () => import('./canqui/canqui.module').then(m => m.CanquiModule) },
+  { path: '', loadChildren: () => import('./canqui/canqui.module').then(m => m.CanquiModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
